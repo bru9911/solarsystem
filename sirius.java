a951246076f2ba6ba766bea9ebe75dfa5d295b8d
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2024-06-17 19:14:52 -0300

    Create sirius.java

---
+++ sirius.java
@@ -0,0 +1,15 @@
+public class EstrelaSirius {
+    public static void main(String[] args) {
+        // Informações sobre a estrela Sirius
+        String nome = "Sirius";
+        String tipo = "Estrela binária";
+        double temperaturaKelvin = 9940; // temperatura em Kelvin
+        double distanciaAnosLuz = 8.6; // distância da Terra em anos-luz
+
+        // Imprimindo informações
+        System.out.println("Informações sobre a estrela " + nome + ":");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Temperatura: " + temperaturaKelvin + " Kelvin");
+        System.out.println("Distância da Terra: " + distanciaAnosLuz + " anos-luz");
+    }
+}
