5ab02d41f9577d6fc3033576537c860d1577905e
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2024-06-17 19:13:44 -0300

    Create andromeda.java

---
+++ andromeda.java
@@ -0,0 +1,15 @@
+public class GalaxiaAndromeda {
+    public static void main(String[] args) {
+        // Informações sobre a Galáxia de Andrômeda
+        String nome = "Andrômeda";
+        double diametroAnosLuz = 220000; // diâmetro em anos-luz
+        double distanciaDaTerraAnosLuz = 2500000; // distância da Terra em anos-luz
+        String tipo = "Espiral"; // tipo de galáxia
+
+        // Imprimindo informações
+        System.out.println("Informações sobre a galáxia " + nome + ":");
+        System.out.println("Diâmetro: " + diametroAnosLuz + " anos-luz");
+        System.out.println("Distância da Terra: " + distanciaDaTerraAnosLuz + " anos-luz");
+        System.out.println("Tipo: " + tipo);
+    }
+}
