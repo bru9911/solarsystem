dd41b9951c274760d9bd5bd9729eab1bb593854f
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2024-06-17 19:12:25 -0300

    Create terra.java

---
+++ terra.java
@@ -0,0 +1,15 @@
+public class PlanetaTerra {
+    public static void main(String[] args) {
+        // Informações sobre o planeta Terra
+        String nome = "Terra";
+        double diametroKm = 12742; // diâmetro em quilômetros
+        int idadeAnos = 4500000000; // idade aproximada em anos
+        double temperaturaMediaC = 15; // temperatura média em graus Celsius
+
+        // Imprimindo informações
+        System.out.println("Informações sobre o planeta " + nome + ":");
+        System.out.println("Diâmetro: " + diametroKm + " quilômetros");
+        System.out.println("Idade aproximada: " + idadeAnos + " anos");
+        System.out.println("Temperatura média: " + temperaturaMediaC + " graus Celsius");
+    }
+}
