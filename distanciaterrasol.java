0267a6ad28393a99c25fa331a74b5a78545128b0
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2024-06-27 16:56:16 -0300

    Create distanciaterrasol.java

---
+++ distanciaterrasol.java
@@ -0,0 +1,9 @@
+public class DistanciaTerraSol {
+
+    public static void main(String[] args) {
+        // Distância média Terra-Sol em unidades astronômicas (AU)
+        double distanciaMediaTerraSolAU = 1.0;
+
+        System.out.println("A distância média entre a Terra e o Sol é aproximadamente " + distanciaMediaTerraSolAU + " UA.");
+    }
+}
